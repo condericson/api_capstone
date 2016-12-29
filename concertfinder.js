36fcdb694651c19046f3fc3199f558c57b32fc5d
Connor's laptop <condericson@gmail.com>
2016-12-28 22:05:32 -0500

    css edits 1

--- concertfinder.js
+++ concertfinder.js
@@ -1,21 +1,46 @@
 $(document).ready(function() {
 
-/*//Date Formatting function
-Date.prototype.getFromFormat = function(format) {
-  var yyyy = this.getFullYear().toString();
-  format = format.replace(/yyyy/g, yyyy)
-  var mm = (this.getMonth()+1).toString();
-  format = format.replace(/mm/g, (mm[1]?mm:"0"+mm[0]));
-  var dd  = this.getDate().toString();
-  format = format.replace(/dd/g, (dd[1]?dd:"0"+dd[0]));
-  var hh = this.getHours().toString();
-  format = format.replace(/hh/g, (hh[1]?hh:"0"+hh[0]));
-  var ii = this.getMinutes().toString();
-  format = format.replace(/ii/g, (ii[1]?ii:"0"+ii[0]));
-  var ss  = this.getSeconds().toString();
-  format = format.replace(/ss/g, (ss[1]?ss:"0"+ss[0]));
-  return format;
-};*/
+
+
+
+
+//SPOTIFY SEARCH AND API FUNCTIONS
+
+/*
+
+//Spotify Api url and key
+/*var spotifyApp = {
+	spotifyApi: 'https://api.spotify.com',
+	spotifyApiKey: '61057b84405e46708ec8db976c69e093'
+};
+var SPOTIFY_BASE_URL = 'https://api.spotify.com';
+
+function getDataFromApi(addressEntry, citystatezipEntry, callback) {
+	var query = {
+		'zws-id': 'cbbf4a55f6a7486bb203a7efc97ada5c',
+		address: addressEntry,
+		citystatezip: citystatezipEntry
+	}
+	$.getJSON(ZILLOW_BASE_URL, query, callback);
+}
+
+function displayZillowSearchData(data) {
+	console.log(data);
+}
+
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(e) {
+    e.preventDefault();
+    var addressEntry = $(this).find('.js-address').val();
+    var citystatezipEntry = $(this).find('.js-zip').val();
+    getDataFromApi(addressEntry, citystatezipEntry, displayZillowSearchData);
+  });
+}
+
+$(function(){watchSubmit();});
+*/
+
 
 
 
@@ -29,7 +54,7 @@ var jambaseApp = {
 };
 
 //Event listener for displaying events of searched artist
-$('.js-artist-search-form').submit(function(event) {
+$('.js-search-form').submit(function(event) {
 	event.preventDefault();
 	console.log($('#js-artist').val())
 	getJambaseArtistId(jambaseApp, $('#js-artist').val());	
@@ -64,7 +89,15 @@ function getJambaseEvents(jambaseApp, artistId) {
 	}
 	$.getJSON(url, params, function(data){
 		console.log(data);
-		displayEvents(data);
+		if (data.Events.length == 0){
+			$('.eventlist').append(
+			'<p id="noresults">No results found</p>'
+			);
+		}
+		else {
+			displayEvents(data);	
+		}
+		
 	})
 }
 
@@ -76,7 +109,7 @@ function displayEvents(data) {
 	eventDetails.forEach(function(object) {
 		//var dateStamp = new Date(object.Date);
 		$('.eventlist').append(
-			'<ul>' + 
+			'<ul class="eventitem">' + 
 			'<li>' + moment(object.Date).format("MMMM Do YYYY") + '</li>' + 
 			'<li>' + object.Venue.Name + '</li>' + 
 			'<li>' + object.Venue.Address + '</li>' + 
@@ -85,8 +118,8 @@ function displayEvents(data) {
 			'<li>' + object.Venue.ZipCode + '</li>' +
 			'<li class="lat">' + object.Venue.Latitude + '</li>' +
 			'<li class="lng">' + object.Venue.Longitude + '</li>' +
-			'</ul>' + 
-			'<button class="mapbutton" type="submit">Map it!</button>'
+			'<button class="mapbutton" type="submit">Map it!</button>' +
+			'</ul>'			
 			);
 	})
 		
@@ -129,41 +162,6 @@ function initMap(button) {
 
 
 
-
-
-
-
-
-///SPOTIFY SEARCH AND API FUNCTIONS
-
-/*
-var SPOTIFY_BASE_URL = 'https://api.spotify.com';
-
-function getDataFromApi(addressEntry, citystatezipEntry, callback) {
-	var query = {
-		'zws-id': 'cbbf4a55f6a7486bb203a7efc97ada5c',
-		address: addressEntry,
-		citystatezip: citystatezipEntry
-	}
-	$.getJSON(ZILLOW_BASE_URL, query, callback);
-}
-
-function displayZillowSearchData(data) {
-	console.log(data);
-}
-
-
-function watchSubmit() {
-  $('.js-search-form').submit(function(e) {
-    e.preventDefault();
-    var addressEntry = $(this).find('.js-address').val();
-    var citystatezipEntry = $(this).find('.js-zip').val();
-    getDataFromApi(addressEntry, citystatezipEntry, displayZillowSearchData);
-  });
-}
-
-$(function(){watchSubmit();});
-*/
 
 
 
